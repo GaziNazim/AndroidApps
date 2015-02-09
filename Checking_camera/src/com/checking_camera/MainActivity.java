1d484ebcce08f896f2e490a45d4a6349a835da3b
GaziNazim <gazinazim1@gmail.com>
2015-02-09 23:03:52 +0600

    09-02-2015 11:03

--- Checking_camera/src/com/checking_camera/MainActivity.java
+++ Checking_camera/src/com/checking_camera/MainActivity.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -22,6 +24,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -40,11 +43,13 @@ public class MainActivity extends Activity {
 	            mCamera = null;
 	        }
 	    }
-	
+	//  TextView tv = (TextView) findViewById(R.id.textView1);
 	private PictureCallback mPicture= new PictureCallback() {
 		
 		@Override
 		public void onPictureTaken(byte[] data, Camera camera) {
+			TextView tv = (TextView) findViewById(R.id.textView1);
+			
 			// TODO Auto-generated method stub
 			File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
 			if(pictureFile==null){
@@ -55,28 +60,57 @@ public class MainActivity extends Activity {
 				FileOutputStream fos = new FileOutputStream(pictureFile);
 				fos.write(data);
 				fos.close();
+				tv.append(pictureFile.toString());
 			}catch(FileNotFoundException e){
 				Log.d("FileOutputstream","File not found " + e.getMessage());
-				
+				tv.append("NOT");
 			}
 			catch(IOException e)
 			{
 				Log.d("IO Exception","Error accessing file " + e.getMessage());
+				tv.append("NOT");
 			}
-			releaseCamera();
+			//releaseCamera();
 		}
-		private Uri getOutputMediaFileUri(int type){
-		      return Uri.fromFile(getOutputMediaFile(type));
-		}
-
-		private File getOutputMediaFile(int type) {
-			// TODO Auto-generated method stub
-			File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-		              Environment.DIRECTORY_PICTURES), "MyCameraApp");
-			return null;
-		}
+		
 	};
-	
+	private static Uri getOutputMediaFileUri(int type){
+	      return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/** Create a File for saving an image or video */
+	private static File getOutputMediaFile(int type){
+	    // To be safe, you should check that the SDCard is mounted
+	    // using Environment.getExternalStorageState() before doing this.
+
+	    File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+	              Environment.DIRECTORY_PICTURES), "MyCameraApp");
+	    // This location works best if you want the created images to be shared
+	    // between applications and persist after your app has been uninstalled.
+
+	    // Create the storage directory if it does not exist
+	    if (! mediaStorageDir.exists()){
+	        if (! mediaStorageDir.mkdirs()){
+	            Log.d("MyCameraApp", "failed to create directory");
+	            return null;
+	        }
+	    }
+
+	    // Create a media file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    File mediaFile;
+	    if (type == MEDIA_TYPE_IMAGE){
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+	        "IMG_"+ timeStamp + ".jpg");
+	    } else if(type == MEDIA_TYPE_VIDEO) {
+	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+	        "VID_"+ timeStamp + ".mp4");
+	    } else {
+	        return null;
+	    }
+
+	    return mediaFile;
+	}
 	private boolean checkCameraHardware(Context context)
 	{
 		if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA))
